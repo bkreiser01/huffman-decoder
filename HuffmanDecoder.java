bd0e01f9f6d38db9a70cb914f4711e78c3b56787
Brandon <bkreiser5@gmail.com>
2019-03-13 17:31:24 -0400

    added from gdrive\

--- HuffmanDecoder.java
+++ HuffmanDecoder.java
@@ -1,4 +1,5 @@
 //Brandon Kreiser
+
 public class HuffmanDecoder
 {
     public static void main(String[] args) throws java.io.IOException
@@ -7,33 +8,34 @@ public class HuffmanDecoder
         BinaryTreeNode<Character> currentNode = tree;
         char currentNum = (char)System.in.read();
 
-        System.out.println("***" + currentNum);
         while(currentNum != (char)(-1))
         {
-            if(currentNum == '0')
-                currentNode = currentNode.getLeftChild();
-            else if(currentNum == '1')
-                currentNode = currentNode.getRightChild();
+            if(currentNode.getData() == null)
+            {
+                if(currentNum == '0')
+                    currentNode = currentNode.getLeftChild();
+                else if(currentNum == '1')
+                    currentNode = currentNode.getRightChild();
+            }
             else
             {
                 System.out.print(currentNode.getData());
                 currentNode = tree;
             }
+            
+            currentNum = (char)System.in.read();
         }
+        
+        System.out.println();
     }
     
     private static BinaryTreeNode<Character> buildTree() throws java.io.IOException
     {
         char next = (char)System.in.read();
-        System.out.println(next);
-        if(next == '1')
-        {
-            char x = (char)System.in.read();
-            BinaryTreeNode<Character> y = new BinaryTreeNode<Character>(x);
-            System.out.println(x +" ==>" + y.getData());
-            return (y);
-        }
-        else
+        
+        if(next == '0')
             return(new BinaryTreeNode<Character>(null, buildTree(), buildTree()));
+        else if(next == '1')
+            return(new BinaryTreeNode<Character>((char)System.in.read()));
     }
 }
