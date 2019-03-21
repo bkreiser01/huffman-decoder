ba3c5cbd22480c4b9849fbd5d653238c8bb9cf8d
Brandon Kreiser <bkreiser5@gmail.com>
2019-03-20 21:54:06 -0400

    final commit

--- HuffmanDecoder.java
+++ HuffmanDecoder.java
@@ -12,23 +12,18 @@ public class HuffmanDecoder
         {
             if(currentNode.getData() == null)
             {
-                system.out.println("Node =>" + currentNode.getData);
                 if(currentNum == '0')
-                {
                     currentNode = currentNode.getLeftChild();
-                }
                 else if(currentNum == '1')
-                {
                     currentNode = currentNode.getRightChild();
-                }
+                    
+                currentNum = (char)System.in.read();
             }
             else
             {
                 System.out.print(currentNode.getData());
                 currentNode = tree;
             }
-            
-            currentNum = (char)System.in.read();
         }
         
         System.out.println();
@@ -38,9 +33,13 @@ public class HuffmanDecoder
     {
         char next = (char)System.in.read();
         
-        if(next == '0')
-            return(new BinaryTreeNode<Character>(null, buildTree(), buildTree()));
-        else
+        if(next == '1')
+        {
             return(new BinaryTreeNode<Character>((char)System.in.read()));
+        }
+        else
+        {
+            return(new BinaryTreeNode<Character>(null, buildTree(), buildTree()));
+        }
     }
 }
