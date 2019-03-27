5eea770fd4c3b831f4c54d112fd0bdf65f196df8
Brandon Kreiser <bkreiser5@gmail.com>
2019-03-27 17:00:12 -0400

    final commit

--- HuffmanDecoder.java
+++ HuffmanDecoder.java
@@ -4,14 +4,16 @@ public class HuffmanDecoder
 {
     public static void main(String[] args) throws java.io.IOException
     {
-        BinaryTreeNode<Character> tree = buildTree();
-        BinaryTreeNode<Character> currentNode = tree;
+        BinaryTreeNode<Character> root = buildTree();
+        BinaryTreeNode<Character> currentNode = root;
         char currentNum = (char)System.in.read();
 
+        //While there are numbers in the file, search through the tree for the proper characters.
         while(currentNum != (char)(-1))
         {
             if(currentNode.getData() == null)
             {
+                
                 if(currentNum == '0')
                     currentNode = currentNode.getLeftChild();
                 else if(currentNum == '1')
@@ -21,14 +23,16 @@ public class HuffmanDecoder
             }
             else
             {
+                //When a character is found, print it and go back to the root.
                 System.out.print(currentNode.getData());
-                currentNode = tree;
+                currentNode = root;
             }
         }
         
         System.out.println();
     }
     
+    //Recurively reconstruct the tree.
     private static BinaryTreeNode<Character> buildTree() throws java.io.IOException
     {
         char next = (char)System.in.read();
